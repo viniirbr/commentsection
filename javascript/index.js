7bd26af02dcb51dbf8110245172745412f483cb7
Vinicius Ribeiro <vayne99@gmail.com>
2022-02-07 21:38:07 -0300

    Exibir comentarios do arquivo JSON

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 import { addComment }  from './addComment.js'
+import { loadComments } from './loadComments.js'
 
 const button = document.querySelector('.button')
-
+loadComments()
 button.addEventListener('click', addComment)
---
+++ javascript/loadComments.js
@@ -0,0 +1,51 @@
+const commentsList = document.getElementById('comment__list')
+
+export const loadComments = () => {
+    fetch("../data.json")
+    .then(response => {
+    return response.json();
+})
+.then(jsondata => {
+    var commentsJSON = jsondata['comments']
+    for(var comment in commentsJSON) {
+        var currentComment = commentsJSON[comment]
+        var currentUser = currentComment['user']
+        const article = document.createElement('article')
+        article.classList.add('comment-item')
+        article.classList.add('comment-block')
+        article.innerHTML = 
+        `
+        <div class="like-count-container">
+          <div class="like-control-component">
+            <img src="images/icon-plus.svg" class="add-remove-like">
+            <p class="like-count">${currentComment.score}</p>
+            <img src="images/icon-minus.svg" class="add-remove-like">
+          </div>
+        </div>
+        <div class="comment-item-container">
+          <div class="comment-item-header">
+            <div class="container-author-date">
+              <img class="author-profile" src="images/avatars/image-${currentUser.username}.png">
+              <h3 class="author">
+              ${currentUser.username}
+              </h3>
+              <h4 class="date">
+                ${currentComment.createdAt}  
+              </h4>
+            </div>
+            <div class="comment-item-header-reply">
+              <img src="images/icon-reply.svg" class="reply-icon">
+              <p class="reply-text">Reply</p>
+            </div>
+
+          </div>
+          <p class="comment-item-text">
+            ${currentComment.content}  
+          </p>  
+        </div>
+        `
+        commentsList.append(article)
+        console.log(currentComment['user'].username)
+    }
+})
+}
